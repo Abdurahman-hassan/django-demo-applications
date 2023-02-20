9346c8cb231803f69f1268819a73b6d27ff69fb3
abdurahman-hassan <abdelrahmanhassan@paymob.com>
2023-02-20 04:49:22 +0200

    update models, admin files

--- demoApp/admin.py
+++ demoApp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import MenuCategory, Menu
 # Register your models here.
+
+admin.site.register(MenuCategory)
+admin.site.register(Menu)
--- demoApp/models.py
+++ demoApp/models.py
@@ -2,8 +2,25 @@ from django.db import models
 
 
 # Create your models here.
+
+class MenuCategory(models.Model):
+    """Model representing a menu category."""
+    category_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.category_name
+
+
 class Menu(models.Model):
-    dish_name = models.CharField(max_length=100)
+    """Model representing a dish in the menu."""
+    dish_name = models.CharField(max_length=80)
     dish_description = models.TextField()
-    dish_price = models.IntegerField()
+    dish_prices = models.IntegerField()
+    category_id = models.ForeignKey(MenuCategory,
+                                    on_delete=models.PROTECT,
+                                    default=None,
+                                    related_name='menu_category')
     # dish_image = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.dish_name + " : " + self.dish_description + " -> " + str(self.dish_prices)
