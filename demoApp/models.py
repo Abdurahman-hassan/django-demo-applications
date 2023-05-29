c98631c462a561679ce4d611a8d5ec89f0ec02da
abdurahman-hassan <abdelrahman15376@fagr.bu.edu.eg>
2023-05-29 17:00:15 +0300

    Add Demo Model

--- demoApp/models.py
+++ demoApp/models.py
@@ -24,3 +24,23 @@ class Menu(models.Model):
 
     def __str__(self):
         return self.dish_name + " : " + self.dish_description + " -> " + str(self.dish_prices)
+
+
+class Demo(models.Model):
+    """Model representing a demo."""
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    address = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    zip = models.CharField(max_length=100)
+    phone = models.CharField(max_length=100)
+    comments = models.TextField()
+    favorit_dishes = models.CharField(max_length=100)
+    favorit_dishes2 = models.CharField(max_length=100)
+    favorit_dishes3 = models.CharField(max_length=100)
+
+    class Meta:
+        permissions = [('can_change_phone', 'Can change phone')]
+    def __str__(self):
+        return self.name + " : " + self.email + " -> " + self.phone
