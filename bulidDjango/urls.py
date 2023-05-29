e9c1e68c4b18252d6b5665bebf42d5e1ad299947
abdurahman-hassan <abdelrahman15376@fagr.bu.edu.eg>
2023-05-29 17:01:00 +0300

    Update Urls

--- bulidDjango/urls.py
+++ bulidDjango/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -22,6 +24,6 @@ urlpatterns = [
     path('demo/', include('demoApp.urls')),
     path('admin/', admin.site.urls),
     path('test/', views.test_page, name='test_page'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 handler404 = 'bulidDjango.views.handler404'
--- demoApp/urls.py
+++ demoApp/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('test/', views.test_view, name='test_view'),
     path('test_form/', views.TestForm.as_view(), name='test_form'),
+    path('test_model_form/', views.DemoModelFormView.as_view(), name='test_model_form'),
     path('display_date/', views.display_date, name='display_date'),
     path('testmethods/', views.MyView.as_view(), name='testmethods'),
     # path('getuser/<str:name>/<int:id>', views.pathview, name='pathview'),
@@ -19,9 +20,14 @@ urlpatterns = [
     path('getform/', views.getform, name="redirect_get_form_data"),
     path('getdish/<str:dish_name>', views.custom_path_view, name='custompath'),
     path('permanent_redirect/', views.make_user_permanent_redirect, name='permanent_redirect'),
-
+    path('products/', views.ProductListView.as_view(), name='product_detail'),
     # regex
     re_path(r'^getuser/(?P<name>[a-z]+)/(?P<id>[0-9]{1,2})/$', views.pathview, name='pathview'),
     re_path(r'^use_regex/getuser/([0-9]{1,2})/$', views.pathview2, name='pathview'),
+    path('products/', views.ProductListView.as_view(), name='products'),
+
+    # url(r'^users_only/', login_required(myview)),
+    #
+    # url(r'^category/', permission_required('myapp.change_category', login_url='login')(myview)),
 
 ]
